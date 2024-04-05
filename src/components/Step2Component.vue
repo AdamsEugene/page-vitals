d51a091efbcc78c91ef3beeded3ec86a4e20d607
Isaac Forson <youngsonf2@gmail.com>
2024-04-05 17:16:10 +0000

    saving step 2 component initial layout

--- src/components/Step2Component.vue
+++ src/components/Step2Component.vue
@@ -1,32 +1,93 @@
 <template>
   <div class="page_vitals_step1_content_wrapper">
-    <a class="page_vitals_link">www.cadenlane.org</a>
-    <h2 class="page_vitals_heading">Site Speed</h2>
+    <!-- ################## Header Wrapper ###################### -->
+    <section>
+      <div class="page_vitals_top_header_wrapper">
+        <div>
+          <a class="page_vitals_link">www.cadenlane.org</a>
+          <h2 class="page_vitals_heading">Site Speed</h2>
+        </div>
+        <div id="page_vitals_tab_button_wrapper">
+          <button class="page_vitals_tab_button page_vitals_tab_button_left" :class="{ active: activeTab === 'mobile' }" @click="changeTab('mobile')">
+            <svg class="page_vitals_tab_icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <path :stroke="activeTab === 'mobile' ? 'white' : 'black'" stroke-width="1.5" d="M17 2H7C5.89543 2 5 2.89543 5 4V20C5 21.1046 5.89543 22 7 22H17C18.1046 22 19 21.1046 19 20V4C19 2.89543 18.1046 2 17 2Z" stroke-linecap="round" stroke-linejoin="round"/>
+          <path :stroke="activeTab === 'mobile' ? 'white' : 'black'" stroke-width="1.5" d="M12 18H12.01" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+            <p class="page_vitals_tab_description">Mobile</p>
+          </button>
+          <button class="page_vitals_tab_button page_vitals_tab_button_right" :class="{ active: activeTab === 'desktop' }" @click="changeTab('desktop')">
+            <svg class="page_vitals_tab_icon" xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+            <path :stroke="activeTab === 'desktop' ? 'white' : 'black'" stroke-width="1.5" d="M19 4.5H5C3.89543 4.5 3 5.39543 3 6.5V14.5C3 15.6046 3.89543 16.5 5 16.5H19C20.1046 16.5 21 15.6046 21 14.5V6.5C21 5.39543 20.1046 4.5 19 4.5Z" stroke-linecap="round" stroke-linejoin="round"/>
+            <path :stroke="activeTab === 'desktop' ? 'white' : 'black'" stroke-width="1.5" d="M2 20.5H22" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+            <p class="page_vitals_tab_description">Desktop</p>
+          </button>
+        </div>
+
+      </div>
+    </section>
+    
+    <!--################## Mobile Section Tab ######################-->
+    <div class="page_vitals_tab1_content" v-show="activeTab === 'mobile'">
+      <div class="page_vitals_tab1_content_header_wrapper">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <path d="M10 2.5L8.40667 7.34417C8.32512 7.59209 8.1865 7.8174 8.00195 8.00195C7.8174 8.1865 7.59209 8.32512 7.34417 8.40667L2.5 10L7.34417 11.5933C7.59209 11.6749 7.8174 11.8135 8.00195 11.9981C8.1865 12.1826 8.32512 12.4079 8.40667 12.6558L10 17.5L11.5933 12.6558C11.6749 12.4079 11.8135 12.1826 11.9981 11.9981C12.1826 11.8135 12.4079 11.6749 12.6558 11.5933L17.5 10L12.6558 8.40667C12.4079 8.32512 12.1826 8.1865 11.9981 8.00195C11.8135 7.8174 11.6749 7.59209 11.5933 7.34417L10 2.5Z" stroke="#016F52" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M15.833 2.5V5.83333" stroke="#016F52" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M15.833 14.1667V17.5" stroke="#016F52" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M14.167 4.16666H17.5003" stroke="#016F52" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M14.167 15.8333H17.5003" stroke="#016F52" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <span class="page_vitals_tab1_content_header">Mobile Recommendations</span>
+      </div>
+
+      <div class="page_vitals_info_wrapper">
+        <p class="page_vitals_info_text">To improve your site speed, consider the following:</p>
+
+        <div class="page_vitals_tab1_content_header_wrapper">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+          <g clip-path="url(#clip0_1278_5460)">
+          <path d="M16.667 6.66666H8.33366C7.41318 6.66666 6.66699 7.41285 6.66699 8.33332V16.6667C6.66699 17.5871 7.41318 18.3333 8.33366 18.3333H16.667C17.5875 18.3333 18.3337 17.5871 18.3337 16.6667V8.33332C18.3337 7.41285 17.5875 6.66666 16.667 6.66666Z" stroke="#016F52" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M3.33366 13.3333C2.41699 13.3333 1.66699 12.5833 1.66699 11.6667V3.33332C1.66699 2.41666 2.41699 1.66666 3.33366 1.66666H11.667C12.5837 1.66666 13.3337 2.41666 13.3337 3.33332" stroke="#016F52" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          </g>
+          <defs>
+          <clipPath id="clip0_1278_5460">
+          <rect width="20" height="20" fill="white"/>
+          </clipPath>
+          </defs>
+          </svg>
+        <span class="page_vitals_tab1_content_header">Copy All</span>
+      </div>
+      </div>
+
+      <div class="page_vitals_button_wrapper page_vitals_tab1_content_wrapper">
+        <p class="page_vitals_content_ul_heading">Compress and resize images to reduce their file size without sacrificing quality.</p>
+    </div>
+      <div class="page_vitals_button_wrapper page_vitals_tab1_content_wrapper page_vitals_tab1_content_margin">
+        <p class="page_vitals_content_ul_heading">Reduce unused JavaScript and defer loading scripts until they are required to decrease bytes consumed by network activity.</p>
+    </div>
+      <div class="page_vitals_button_wrapper page_vitals_tab1_content_wrapper page_vitals_tab1_content_margin">
+        <p class="page_vitals_content_ul_heading">Eliminate render-blocking resources.</p>
+    </div>
+    </div>
+
+    <!--################## Desktop Section Tab ####################-->
+    <div class="page_vitals_tab2_content" v-show="activeTab === 'desktop'">desktop content</div>
+
 </div>
 </template>
 
 <script>
 export default {
-data() {
-  return {
-    thumbsUpClicked: false,
-    thumbsDownClicked: false
-  };
-},
-methods: {
-  handleThumbsUpClick() {
-    this.thumbsUpClicked = !this.thumbsUpClicked;
-    if (this.thumbsUpClicked) {
-      this.thumbsDownClicked = false; // Unselect thumbs down
-    }
+  data() {
+    return {
+      activeTab: 'mobile' // Initially show mobile content
+    };
   },
-  handleThumbsDownClick() {
-    this.thumbsDownClicked = !this.thumbsDownClicked;
-    if (this.thumbsDownClicked) {
-      this.thumbsUpClicked = false; // Unselect thumbs up
+  methods: {
+    changeTab(tab) {
+      this.activeTab = tab;
     }
   }
-}
 };
 </script>
 
@@ -64,35 +125,99 @@ methods: {
   font-weight: 600;
 }
 
-.page_vitals_content_ul_heading {
-  color: var(--Grey-800, var(--Grey-800, #34404B));
-  font-size: 14px;
-  font-weight: 500;
-  line-height: 24px;
+.page_vitals_top_header_wrapper{
+display: flex;
+justify-content: space-between;
+/* background-color: red; */
 }
 
-.page_vitals_content_li {
-  margin-bottom: 10px;
+#page_vitals_tab_button_wrapper{
+  display: flex;
+  box-shadow: 0px 1px 2px 0px rgba(26, 40, 53, 0.09);
+  margin-bottom: 0px;
+  height: 60px;
 }
-
-.thumbs-up-active svg {
-stroke: #12904E; 
-stroke-width: 0.5px;
-}
-
-#page_vitals_thumbs_up{
-margin-right: 10px;
-}
-
-.thumbs-down-active svg {
-stroke: #B71E2D;
-stroke-width: 0.5px;
-}
-
-/* Style SVG icons */
-svg {
-width: 24px;
-height: 24px; 
+.page_vitals_tab_button{
+  display: flex;
+height: 60px;
+padding: 6px 35px;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: 6px;
+flex: 1 0 0;
+background-color: #FFF;
+border: none;
 cursor: pointer;
 }
+
+.page_vitals_tab_button_left{
+border-radius: 10px 0px 0px 10px;
+}
+.page_vitals_tab_button_right{
+  border-radius: 0px 10px 10px 0px;
+}
+
+.page_vitals_tab_description{
+  font-size: 12px;
+  color: var(--Grey-800, #34404B);
+  margin-bottom: 0px;
+  font-weight: 500;
+  margin-top: 0px;
+}
+
+.page_vitals_tab_icon{
+  width: 30px;
+  height: 30px;
+}
+
+.active {
+  background-color: #00936F;
+  color: #fff !important;
+}
+.active > .page_vitals_tab_description{
+  /* background-color: #016F52; */
+  color: #fff !important;
+}
+.active > svg {
+  color: #fff !important;
+}
+
+.page_vitals_tab1_content_header_wrapper{
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.page_vitals_info_wrapper{
+  display: flex;
+  justify-content: space-between;
+}
+.page_vitals_tab1_content_header{
+  color: #34404B;
+font-size: 14px;
+font-weight: 600;
+margin-left: 6px;
+cursor: pointer;
+}
+
+.page_vitals_info_text{
+  color:  #34404B;
+font-size: 13px;
+font-weight: 500;
+}
+
+.page_vitals_content_ul_heading {
+    color: #34404B;
+    font-size: 14px;
+    font-weight: 500;
+  }
+
+  .page_vitals_tab1_content_wrapper{
+    margin-top: 15px;
+    border: 1px solid var(--Grey-200, #E6E7E8);
+  }
+
+  .page_vitals_tab1_content_margin{
+    margin-top: 10px;
+  }
 </style>
