f06375b4613f6d7afe03b6bb66c6ed3063c7ad3a
Isaac Forson <youngsonf2@gmail.com>
2024-05-20 11:13:09 +0000

    created initial calendar ui and component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import CalendarModal from './components/CalendarModal.vue';
 </script>
 
 <template>
   <div id="app">
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+    <CalendarModal/>
   </div>
 </template>
 
